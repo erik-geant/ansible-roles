04178957212ce608cac8b3a8917275a9e0a17023
Erik Reid <erik.reid@geant.org>
2017-04-25 16:19:41 +0200

    recovered work lost by submodule commit error

---
+++ faucet/files/etc/faucet/faucet-environment.sh
@@ -0,0 +1,11 @@
+# directory containing FAUCET application code
+FAUCET_APP=faucet.faucet
+# file containing Gauge's custom ryu.conf
+FAUCET_RYU_CONF=/etc/faucet/ryu.conf
+# location of FAUCET's configuration file.
+FAUCET_CONFIG=/etc/faucet/faucet.yml
+# where FAUCET should log to
+# (directory must exist and be writable by FAUCET_USER)
+FAUCET_LOG="/var/log/faucet/faucet.log"
+# where FAUCET should log exceptions to (directory must exist as above)
+FAUCET_EXCEPTION_LOG="/var/log/faucet/exception.log"
--- faucet/files/start-faucet.sh
+++ faucet/files/start-faucet.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 . ~faucet/virtenv/bin/activate
-. /etc/default/faucet
+. /etc/faucet/faucet-environment.sh
 
 export FAUCET_CONFIG
 export FAUCET_LOG
 export FAUCET_EXCEPTION_LOG
 
-ryu-manager --config-file=$FAUCET_RYU_CONF --ofp-listen-host=$FAUCET_LISTEN_HOST --ofp-tcp-listen-port=$FAUCET_LISTEN_PORT $FAUCET_APP_DIR/faucet.py
+ryu-manager --config-file=$FAUCET_RYU_CONF  $FAUCET_APP
 
